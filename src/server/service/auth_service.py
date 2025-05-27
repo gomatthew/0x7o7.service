85f8ac08cf1c1d53020fa1aeb68ec3c15205b125
0x7o7 <gomatt6688@gmail.com>
2025-05-27 23:33:08 +0800

    5.27 finish work

--- src/server/service/auth_service.py
+++ src/server/service/auth_service.py
@@ -1,9 +1,11 @@
 from fastapi import Body
 from src.server.dto import ApiCommonResponseDTO
-
+from src.configs import logger
 
 async def user_login(username: str = Body(..., description="用户名"), password: str = Body(..., description="密码")):
     if username == '0x7o7' and password == 'root':
+        logger.info(ApiCommonResponseDTO(message="login success").model_dict())
         return ApiCommonResponseDTO(message="login success").model_dict()
     else:
+        logger.info(ApiCommonResponseDTO(message="login success").model_dict())
         return ApiCommonResponseDTO(message="login error").model_dict()
